717b7e1bc3c5694d5fcacbecc2a2419cc393bf46
Jacob Holmer <yacub90@gmail.com>
2021-01-13 16:55:31 -0800

    extended 03 lab

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
-
+var score = 0
 var userName = prompt('what is your name?')
   alert('Greetings ' + userName + '!')
   
@@ -7,7 +7,8 @@ var question1 = prompt("Is my name Jacob?");
 
     if (question1.toLowerCase() == 'yes') {
       console.log('correct!')
-      alert('correct!')  
+      alert('correct!') 
+      score++ 
     } else {
       console.log('try again')
       alert('try again')
@@ -17,6 +18,8 @@ var question2 = prompt('am I a hairdresser?')
     if (question2.toLowerCase() == 'yes') {
        console.log('correct!')
        alert('correct!')
+          score++
+
      } else {
       console.log('try again')
       alert('try again')
@@ -26,6 +29,7 @@ var question2 = prompt('am I a hairdresser?')
 var  question3 = prompt('have I ever been an apprentice?')
      if (question3.toLowerCase() == 'yes') {
        console.log('correct!')
+       score++
        alert('correct!')
       } else {
         console.log('try again')
@@ -35,6 +39,7 @@ var  question3 = prompt('have I ever been an apprentice?')
 var question4 = prompt('Am I from a small town?')
      if (question4.toLowerCase() == 'yes') {
        console.log('correct!')
+       score++
        alert('correct!')
        } else {
         console.log('try again')
@@ -44,9 +49,52 @@ var question4 = prompt('Am I from a small town?')
 var question5 = prompt('Do I want to be a Software Developer?')
      if (question5.toLowerCase() == 'yes'){
        console.log('correct!')
+       score++
      }  else {
        console.log('try again')
        alert('try again')
      }
 
-alert('thanks for playing' + userName  + 'have a wonderful life')
+
+
+var myAnswer = '7';
+var i = 0
+while ( i < 4 && question6 != myAnswer) {
+  var question6 = prompt('Guess a number between 1 and 10')
+  if (question6 > myAnswer) {
+    alert('too high! try again')
+  }
+  if(question6 < myAnswer) {
+    alert('too low! try again')
+  }
+  if (question6 === myAnswer){
+    alert('correct!')
+    score++
+  }
+  else if (i === 3){
+    alert('too slow, the answer is 7')
+  }
+  i++
+}
+var arrayOfAnswers = ['practical magic', 'avatar' , 'lord of the rings' , 'bennie and june']
+var correctAnswer = false
+for(var j = 0; j < 6; j++) {
+  var question7 = prompt('can you guess one of my top 4 favorite movies?')
+  for(var k = 0; k < arrayOfAnswers.length; k++) {
+    if (question7 === arrayOfAnswers[k]){
+      alert('correct!') 
+      correctAnswer = true
+    }
+  }
+  if (correctAnswer === true){
+    break
+  }
+}
+alert('the correct answers were practical magic, avatar, lord of the rings, or Bennie and June')
+
+score++
+
+
+
+alert(' thanks for playing ' + userName  + ' you got ' + score + ' have a wonderful life')
+ 
